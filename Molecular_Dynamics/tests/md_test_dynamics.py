0e9e8fb20f5b2276706c092cf67fae09c647cf7e
Viinnert <78701051+Viinnert@users.noreply.github.com>
2022-03-24 13:08:29 +0100

    Add files via upload

--- Molecular_Dynamics/tests/md_test_dynamics.py
+++ Molecular_Dynamics/tests/md_test_dynamics.py
@@ -1,15 +1,13 @@
 import sys
 import os
+from md_main import *
+from md_plot import *
+import numpy as np
 
-
-#Workdir path = dir. of main scripts!
+# Workdir path = dir. of main scripts!
 WORKDIR_PATH = os.path.dirname(os.path.realpath(__file__)) + "/../"
 sys.path.insert(1, WORKDIR_PATH)
 
-from md_main import *
-from md_plot import *
-
-np.random.seed(42) #Fix random seed
 
 def simple_init(size):
     i_pos = np.array([[0.3*size[0], 0.51 *size[1]],
@@ -21,36 +19,29 @@ def simple_init(size):
 
 
 if __name__ == "__main__":
-    #def print_usage():
-    #    print("Usage:\n", file=sys.stderr)
-    #    print("Check out usage in README, you gave the wrong number of arguments", file=sys.stderr)
-
-    #required_args = 0
-    #if len(sys.argv) != required_args:
-    #    print_usage()
-    
     # Hardcoded inputs (Maybe replace with argv arguments)
     N_DIM = 2 # Number of dimensions
     N_ATOMS = 2 # Number of particles
-    TEMPERATURE = 100 # Kelvin
+    TEMPERATURE = 1 # Kelvin
     DENSITY = 0.1 # Dimensionless = scaled by m/sigma**n_dim
     ATOM_MASS = 6.6335e-26 # Mass of atoms (kg); Argon = 39.948 u
     POT_ARGS = {'sigma': 3.405e-10, 'epsilon': 119.8} # sigma, epsilon for Argon in units of m and k_B respectively.
 
     # Dimensionless constants
-    CANVAS_ASPECT_RATIO = (1,1) # Canvas size (must be ndarray!)
-    END_OF_TIME = 5 # Maximum time
+    CANVAS_ASPECT_RATIO = (1,1) # Canvas size 
+    END_OF_TIME = 1 # Maximum time
 
-    DELTA_T = 0.1 # Timestep
+    DELTA_T = 0.01 # Timestep
     N_ITERATIONS = int(END_OF_TIME / DELTA_T)
     
-    INIT_MODE = "random"
-    INIT_MODE = lambda: simple_init((N_ATOMS / DENSITY, N_ATOMS / DENSITY))
+    length = (N_ATOMS / DENSITY)**(1/N_DIM)
+    size = np.asarray(CANVAS_ASPECT_RATIO) * length / np.prod(CANVAS_ASPECT_RATIO)
+    INIT_MODE = lambda: simple_init(size)
     
     DATA_PATH = WORKDIR_PATH + "data/" 
     DATA_FILENAME = "trajectories.hdf5"
     
-    #Main simulation procedure
+    # Main simulation procedure
     sim = Simulation(n_atoms_or_unit_cells=N_ATOMS, 
                     atom_mass=ATOM_MASS,
                     density=DENSITY, 
@@ -64,13 +55,14 @@ if __name__ == "__main__":
     
     sim.__simulate__(n_iterations=N_ITERATIONS, delta_t=DELTA_T)
     
-    #Plot:
+    # Plot:
     with hdf.File(DATA_PATH + DATA_FILENAME,'r') as data_file:
-        
-        plot_energy(data_file)
-        
+        animate_trajectories2D(data_file)        
+        plot_energy(data_file) 
         plot_forces(data_file)
         
-        animate_trajectories2D(data_file)
-        #plot_trajectories2D(data_file)
+
+
+        
+
         

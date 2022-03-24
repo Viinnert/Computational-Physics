c2dc4951ad009e30210bdcf4b640790cd71e6faf
Viinnert <78701051+Viinnert@users.noreply.github.com>
2022-03-24 13:07:58 +0100

    Add files via upload

--- Molecular_Dynamics/tests/md_test_gasphase_argon.py
+++ Molecular_Dynamics/tests/md_test_gasphase_argon.py
@@ -1,33 +1,15 @@
 import sys
 import os
+from md_main import *
+from md_plot import *
+import numpy as np
 
-#Workdir path = dir. of main scripts!
+# Workdir path = dir. of main scripts!
 WORKDIR_PATH = os.path.dirname(os.path.realpath(__file__)) + "/../"
 sys.path.insert(1, WORKDIR_PATH)
 
-from md_main import *
-from md_plot import *
 
-np.random.seed(42) #Fix random seed
-
-def simple_init(size):
-    i_pos = np.array([[0.3*size[0], 0.51 *size[1]],
-                      [0.7*size[0], 0.49 *size[1]]])
-    i_veloc = np.array([[1, 0],
-                        [-1, 0]])
-
-    return i_pos, i_veloc
-
-
-if __name__ == "__main__":
-    #def print_usage():
-    #    print("Usage:\n", file=sys.stderr)
-    #    print("Check out usage in README, you gave the wrong number of arguments", file=sys.stderr)
-
-    #required_args = 0
-    #if len(sys.argv) != required_args:
-    #    print_usage()
-    
+if __name__ == "__main__":  
     # Hardcoded inputs (Maybe replace with argv arguments)
     N_DIM = 3 # Number of dimensions
     N_UNIT_CELLS = (3,3,3) # Number of unit cells per dimension
@@ -35,7 +17,7 @@ if __name__ == "__main__":
     POT_ARGS = {'sigma': 3.405e-10, 'epsilon': 119.8} # sigma, epsilon for Argon in units of m and k_B respectively.
 
     # Dimensionless constants
-    CANVAS_ASPECT_RATIO = (1,1,1) # Canvas size (must be ndarray!)
+    CANVAS_ASPECT_RATIO = (1,1,1) # Canvas size
     END_OF_TIME = 2 # Maximum time
     DELTA_T = 0.01 # Timestep
     N_ITERATIONS = int(END_OF_TIME / DELTA_T)
@@ -47,7 +29,7 @@ if __name__ == "__main__":
     DATA_PATH = WORKDIR_PATH + "data/" 
     TRAJ_DATA_FILENAME = "trajectories_fcc.hdf5"
 
-    #Main simulation procedure
+    # Main simulation procedure
     sim = Simulation(n_atoms_or_unit_cells=N_UNIT_CELLS, 
                      atom_mass=ATOM_MASS, 
                      density=DENSITY, 
@@ -60,18 +42,14 @@ if __name__ == "__main__":
     
     sim.__simulate__(n_iterations=N_ITERATIONS, delta_t=DELTA_T)
     
-    #Plot trajectories, energy and forces:
+    # Plot trajectories, energy and forces:
     with hdf.File(DATA_PATH + TRAJ_DATA_FILENAME,'r') as data_file:
-        
+        animate_trajectories3D(data_file)        
         plot_energy(data_file)
-        
         plot_forces(data_file)
-        
-        #animate_trajectories3D(data_file)
-
     
-    #Pair correlation calculation and plotting:
-    SIM_REPETITIONS = 6
+    # Pair correlation calculation and plotting:
+    SIM_REPETITIONS = 4
     REDUCED_N_ITERATIONS = int(N_ITERATIONS / 1)
     
     histogram_list = []
@@ -79,15 +57,15 @@ if __name__ == "__main__":
     
     for r in tqdm(range(SIM_REPETITIONS)):
         temp_sim = Simulation(n_atoms_or_unit_cells=N_UNIT_CELLS, 
-                        atom_mass=ATOM_MASS, 
-                        density=DENSITY, 
-                        temperature=TEMPERATURE,
-                        n_dim=N_DIM, 
-                        canvas_aspect_ratio=CANVAS_ASPECT_RATIO, 
-                        pot_args=POT_ARGS, 
-                        init_mode=INIT_MODE, 
-                        data_path=DATA_PATH, 
-                        data_filename="temp.hdf5")
+                              atom_mass=ATOM_MASS, 
+                              density=DENSITY, 
+                              temperature=TEMPERATURE,
+                              n_dim=N_DIM, 
+                              canvas_aspect_ratio=CANVAS_ASPECT_RATIO, 
+                              pot_args=POT_ARGS, 
+                              init_mode=INIT_MODE, 
+                              data_path=DATA_PATH, 
+                              data_filename="temp.hdf5")
             
         temp_sim.__simulate__(n_iterations=REDUCED_N_ITERATIONS, delta_t=DELTA_T)
         

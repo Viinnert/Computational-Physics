05fabf43f0d933bae2ae5b534fefefef78082ba9
Vincent Krabbenborg <Vincentkrabbenborg@hotmail.com>
2022-04-29 12:51:17 +0200

    Update LatticeBoltzmann.py

--- Hydrodynamics/LatticeBoltzmann.py
+++ Hydrodynamics/LatticeBoltzmann.py
@@ -13,6 +13,19 @@ class LatticeBoltzmann():
         self.unit_vectors = np.array([[-1,1], [0,1], [1,1],
                                       [-1,0], [0,0], [1,0],
                                       [-1,-1], [0,-1], [1,-1]])
+                                      
+        self.weights = np.array([1/36, 1/9, 1/36,
+                                 1/9, 4/9, 1/9,
+                                 1/36, 1/9, 1/36])
+
+    def initialise_field(self):
+        if self.init_method == 'random':
+            field = np.random.random((self.Nx, self.Ny, 9))
+
+        elif self.init_method == '':
+            pass
+        
+        return field
 
     def step(self):
         pass
@@ -22,10 +35,13 @@ class LatticeBoltzmann():
 
     def simulate(self):
         ''' Run simulation '''
-        if self.init_method == 'random':
-            field = np.random.random((self.Nx, self.Ny, 9, self.N_timesteps))
+        field = self.initialise_field()
 
-        elif self.init_method == '':
-            pass
+        densities = np.sum(field, axis=2)
 
-        density = np.sum(field, axis=2)
+        velocities = np.zeros((self.Nx, self.Ny, 9))
+        for i in range(self.Nx): # improve efficiency maybe?
+            for j in range(self.Ny):
+                velocity = field[i,j,:,None] * self.unit_vectors
+                velocity = np.sum(velocity, axis=0)
+                velocities[i,j] = velocity
